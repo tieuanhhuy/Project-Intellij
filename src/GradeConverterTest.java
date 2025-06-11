b4ba2b13e23294650311096a8fb7854dcd9b97fd
tieuanhhuy <tieuanhhuy>
2025-06-11 15:15:43 +0700

    Initial commit

---
+++ src/GradeConverterTest.java
@@ -0,0 +1,15 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GradeConverterTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.Test
+    public void convert() {
+    }
+}
